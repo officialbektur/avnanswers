8fae7a9eaa08174e8cd7e646495fb8d0d070d4c2
officialbektur <82287316+officialbektur@users.noreply.github.com>
2021-09-29 01:17:34 +0600

    Update index.php

--- adminpassword/index.php
+++ adminpassword/index.php
@@ -1,1 +1,11 @@
-Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat, nemo placeat! Est eos numquam libero, et ducimus, blanditiis quia illum, architecto repellendus odit illo ipsum quas assumenda ex hic dicta?
+<!-------------------- Password Avn Answers -------------------->
+<!DOCTYPE html>
+<html>
+<head>
+    < metacharset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta http-equiv="Refresh" content="0; URL=https://avnanswers.000webhostapp.com/adminpassword/">
+</head>
+<body>
+</body>
+</html>
